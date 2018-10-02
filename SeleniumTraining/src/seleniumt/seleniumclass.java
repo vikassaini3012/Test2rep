bd9ef2477dbd8e3873b258e5ed47e0def57b6789
VIKASSAI <VIKASSAI@VIKASSAI03.corp.amdocs.com>
2018-10-02 15:46:42 +0530

    first - second project

---
+++ SeleniumTraining/src/seleniumt/seleniumclass.java
@@ -0,0 +1,22 @@
+package seleniumt;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumclass 
+
+{
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\JavaTraining\\chromedriver_win32\\chromedriver.exe");	
+		ChromeDriver chdriver = new ChromeDriver();
+		
+		chdriver.get("https://www.facebook.com/");
+		chdriver.manage().window().maximize();
+		
+		//chdriver.findElements(By.id("email")).sendkeys()
+	
+	
+	}
+}
